93c8197d7b94d2f2fc305600488c48cfe24ea0e2
ChijunShen <shenchijun@gmail.com>
2016-12-05 21:25:28 +0100

    Add FatFs and FreeRTOS

---
+++ FatFs/diskio.c
@@ -0,0 +1,172 @@
+#include "diskio.h"
+#include "FST_SDDriver_Library.h"
+/*-----------------------------------------------------------------------*/
+/* Inidialize a Drive                                                    */
+
+DSTATUS disk_initialize (
+	BYTE drv				/* Physical drive nmuber (0..) */
+)
+{
+    u8 state;
+    if(drv)
+    {
+        return STA_NOINIT; 
+    }
+    state = SD_Init();
+    if(state == STA_NODISK)
+    {
+        return STA_NODISK;
+    }
+    else if(state != 0)
+    {
+        return STA_NOINIT;  
+    }
+    else
+    {
+        return 0;           
+    }
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Return Disk Status                                                    */
+
+DSTATUS disk_status (
+	BYTE drv		/* Physical drive nmuber (0..) */
+)
+{
+    if(drv)
+    {
+        return STA_NOINIT;  //?????0??
+    }
+//    if(!SD_DET())
+//    {
+//        return STA_NODISK;
+//    }
+    return 0;
+}
+
+
+/*-----------------------------------------------------------------------*/
+/* Write Sector(s)                                                       */
+
+DRESULT disk_write (
+	BYTE drv,			/* Physical drive nmuber (0..) */
+	const BYTE *buff,	/* Data to be written */
+	DWORD sector,		/* Sector address (LBA) */
+	BYTE count			/* Number of sectors to write (1..255) */
+)
+{
+	uint8_t res;
+
+    if(count == 1)
+    {
+        res = SD_WriteSingleBlock(sector, buff);
+    }
+    else
+    {
+        res = SD_WriteMultiBlock(sector, buff, count);
+    }
+    if(res == 0)
+    {
+        return RES_OK;
+    }
+    else
+    {
+        return RES_ERROR;
+    }
+}
+
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Miscellaneous Functions                                               */
+
+DRESULT disk_ioctl (
+	BYTE drv,		/* Physical drive nmuber (0..) */
+	BYTE ctrl,		/* Control code */
+	void *buff		/* Buffer to send/receive control data */
+)
+{
+    DRESULT res;
+
+
+    if (drv)
+    {    
+        return RES_PARERR;  //????????,????????
+    }
+    
+    //FATFS????????CTRL_SYNC,GET_SECTOR_COUNT,GET_BLOCK_SIZ????
+    switch(ctrl)
+    {
+    case CTRL_SYNC:
+        SD_CS_ENABLE();
+        if(SD_WaitReady()==0)
+        {
+            res = RES_OK;
+        }
+        else
+        {
+            res = RES_ERROR;
+        }
+        SD_CS_DISABLE();
+        break;
+        
+    case GET_BLOCK_SIZE:
+        *(WORD*)buff = 512;
+        res = RES_OK;
+        break;
+
+    case GET_SECTOR_COUNT:
+        *(DWORD*)buff = SD_GetCapacity();
+        res = RES_OK;
+        break;
+    default:
+        res = RES_PARERR;
+        break;
+    }
+
+    return res;
+}
+
+/*-----------------------------------------------------------------------*/
+/* User defined function to give a current time to fatfs module          */
+/* 31-25: Year(0-127 org.1980), 24-21: Month(1-12), 20-16: Day(1-31) */                                                                                                                                                                                                                                          
+/* 15-11: Hour(0-23), 10-5: Minute(0-59), 4-0: Second(0-29 *2) */                                                                                                                                                                                                                                                
+DWORD get_fattime (void)
+{
+  return 0;
+}
+
+/*-----------------------------------------------------------------------*/
+/* Read Sector(s)                                                        */
+
+
+
+DRESULT disk_read (
+	BYTE drv,		/* Physical drive nmuber (0..) */
+	BYTE *buff,		/* Data buffer to store read data */
+	DWORD sector,	/* Sector address (LBA) */
+	BYTE count		/* Number of sectors to read (1..255) */
+)
+{
+		uint8_t res=0;
+    if(count==1)            //1?sector????      
+    {                    
+        res = SD_ReadSingleBlock(sector, buff);      
+    }                                                
+    else                    //??sector????     
+    {                                                
+        res = SD_ReadMultiBlock(sector, buff, count);
+    }                                                
+    if(res == 0x00)
+    {
+        return RES_OK;
+    }
+    else
+    {
+        return RES_ERROR;
+    }
+}
